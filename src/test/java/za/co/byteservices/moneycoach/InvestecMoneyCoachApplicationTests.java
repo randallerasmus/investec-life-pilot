c9033d32def02222a58540c06b63c3b14af9b82c
randall.erasmus <randall.erasmus@interfront.co.za>
2026-05-03 19:31:12 +0200

    Initial commit

---
+++ src/test/java/za/co/byteservices/moneycoach/InvestecMoneyCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.byteservices.moneycoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestecMoneyCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
